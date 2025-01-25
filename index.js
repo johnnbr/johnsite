a7d39214298ad2e13dc8849e700e91410f05116a
john <133288153+johnnbr@users.noreply.github.com>
2025-01-25 16:40:48 -0500

    Changed index.js

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-const http = required('http')
-const port = 3000
+var http = require('http');
 
-const server = http.createServer(function(req, res) {
-  res.write('Hello Node')
-  res.end()
-})
-
-server.listen(port, function(error) {
-  if (error) {
-    console.log('Error', error)
-  } else {
-    console.log('Listening on port ' + port)
-  }
-
-})
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World');
+}).listen(8080);
